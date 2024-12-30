6e0144956034b0e84637953efd79a3bc7322f70f
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-30 18:39:32 +0530

    Update config.py

--- AvishaRobot/config.py
+++ AvishaRobot/config.py
@@ -4,68 +4,68 @@ class Config(object):
 
     #####
 
-    ANILIST_CLIENT = getenv("ANILIST_CLIENT", "8679")
+    ANILIST_CLIENT = "8679"
   
-    ANILIST_SECRET = getenv("ANILIST_SECRET", "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN")
+    ANILIST_SECRET = "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN"
   
-    API_ID = getenv("API_ID", None)
+    API_ID = 22731333
    
-    API_HASH = getenv("API_HASH",None)
+    API_HASH = "f8f993acb1d5f51c950ba873404d9af5"
    
-    TOKEN = getenv("TOKEN", None)
+    TOKEN = "8154928303:AAHOdHIV_PAMiXzLRhmILYPrw7RSw3cdQi8"
   
-    OWNER_ID = getenv("OWNER_ID", "6195725562") 
+    OWNER_ID = "5458968679"
 
-    OWNER_USERNAME = ("OWNER_USERNAME", "loco_x_Pilot")
+    OWNER_USERNAME = "@i_am_morish"
     
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "the_friendz")
+    SUPPORT_CHAT = "PiratesMainchat"
    
-    START_IMG = getenv("START_IMG", "https://graph.org/file/eaa3a2602e43844a488a5.jpg")
+    START_IMG = "https://files.catbox.moe/scntdi.jpg"
 
-    JOIN_LOGGER = getenv("JOIN_LOGGER", "-1002080453210")
+    JOIN_LOGGER = "-1002052732769"
    
-    EVENT_LOGS = getenv("EVENT_LOGS",  "-1002080453210")
+    EVENT_LOGS = "-1002052732769"
   
-    ERROR_LOGS = getenv("ERROR_LOGS", "-1001929735324")
+    ERROR_LOGS = "-1002295880117"
 
-    MONGO_DB_URI= getenv("MONGO_DB_URI", None)
+    MONGO_DB_URI= "mongodb+srv://Gojo_tu:Gojo2347@gojo.eii7p.mongodb.net/?retryWrites=true&w=majority&appName=Gojo"
    
-    LOG_CHANNEL = getenv("LOG_CHANNEL", "-1001852000392")
+    LOG_CHANNEL = "-1002286294709"
    
-    BOT_USERNAME = getenv("BOT_USERNAME" , "avishaBot")
+    BOT_USERNAME = "NoelleSilva_robot"
    
-    DATABASE_URL = getenv("DATABASE_URL", None)
+    DATABASE_URL = "postgres://oocekooc:W2w0GdYOHNwvqKh047VMGjhHq_Xlb2sS@hansken.db.elephantsql.com/oocekooc"
 
-    CASH_API_KEY = getenv("CASH_API_KEY", "V48U2FLLKRHSVD4X")
+    CASH_API_KEY = "V48U2FLLKRHSVD4X"
     
-    TIME_API_KEY = getenv("TIME_API_KEY", "1CUBX1HXGNHW")
+    TIME_API_KEY = "1CUBX1HXGNHW"
 
-    SPAMWATCH_API = getenv("SPAMWATCH_API", "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c")
+    SPAMWATCH_API = "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c"
     
-    SPAMWATCH_SUPPORT_CHAT = getenv("SPAMWATCH_SUPPORT_CHAT", "h_cc_help")
+    SPAMWATCH_SUPPORT_CHAT = "+pexefpzqVXBjNzY1"
     
-    WALL_API = getenv("WALL_API", "2455acab48f3a935a8e703e54e26d121")
+    WALL_API = "2455acab48f3a935a8e703e54e26d121"
     
-    REM_BG_API_KEY = getenv("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
+    REM_BG_API_KEY = "xYCR1ZyK3ZsofjH7Y6hPcyzC"
     
-    OPENWEATHERMAP_ID = getenv("OPENWEATHERMAP_ID", "887da2c60d9f13fe78b0f9d0c5cbaade")
+    OPENWEATHERMAP_ID = "887da2c60d9f13fe78b0f9d0c5cbaade"
 
-    BAN_STICKER = getenv("BAN_STICKER", "CAACAgEAAxkBAAIrTWYljyX_lqcubkAzg0jy45CRvxAFAAKvAgACrLHoRU50VVvh3xWwNAQ")
+    BAN_STICKER = "CAACAgEAAxkBAAIrTWYljyX_lqcubkAzg0jy45CRvxAFAAKvAgACrLHoRU50VVvh3xWwNAQ"
 
-    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+    HEROKU_APP_NAME = None
 
-    HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+    HEROKU_API_KEY = None
 
-    LASTFM_API_KEY = getenv("LASTFM_API_KEY", "8f3315b5806c21004b2822f07825187d")
+    LASTFM_API_KEY = "8f3315b5806c21004b2822f07825187d"
     
     # Optional fields
     
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [5458968679]  # User id of sudo users
+    DEV_USERS = [7078181502]  # User id of dev users
+    DEMONS = [7363537003]  # User id of support users
+    TIGERS = [7078181502]  # User id of tiger users
+    WOLVES = [7078181502]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
